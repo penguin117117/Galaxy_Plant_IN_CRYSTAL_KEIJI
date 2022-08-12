5af33fcef4b74ceed0862e3fe99d1945dc4bbac6
Eigen <disukusisutemu@gmail.com>
2022-08-12 20:42:45 +0900

    iroiro

---
+++ Galaxy Plant InＣＲＹＳＴＡＬ・ケイジ/IO/FileFormat/rarc/rarc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Plant_InＣＲＹＳＴＡＬ_ケイジ.IO.FileFormat.arc
+{
+    internal class rarc
+    {
+        public rarc(string tergetFileName)
+        {
+
+        }
+
+        internal void FileInport(string tergetFileName)
+        {
+        }
+    }
+}
