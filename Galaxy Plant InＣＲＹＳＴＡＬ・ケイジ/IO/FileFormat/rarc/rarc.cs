25ea0f351b1ce1444f2e0d541bd8020dfc900052
Eigen <disukusisutemu@gmail.com>
2022-08-14 22:19:09 +0900

    rarc function add

--- Galaxy Plant InＣＲＹＳＴＡＬ・ケイジ/IO/FileFormat/rarc/rarc.cs
+++ Galaxy Plant InＣＲＹＳＴＡＬ・ケイジ/IO/FileFormat/rarc/rarc.cs
@@ -16,13 +16,55 @@ namespace Galaxy_Plant_InＣＲＹＳＴＡＬ_ケイジ.IO.FileFormat.rarc
         internal string stringTable;
         internal string fileDataSection;
 
+
         public rarc(string tergetFileName)
         {
 
         }
 
-        internal void FileInport(string tergetFileName)
+        //io arc
+        internal void ArchiveFileInport()
         {
+
+        }
+
+        internal void ArchiveFileOutport()
+        {
+
+        }
+
+        //io file
+        internal void AddFile(string TergetFileName)
+        {
+
+        }
+
+        internal void DeleteFile(string TergetFileName)
+        {
+
+        }
+
+        //io dir
+        internal void AddDirectory(string TergetDir)
+        {
+            DirectoryNumbCtl(1);
+        }
+
+        internal void DeleteDirectory(string TergetDir)
+        {
+            DirectoryNumbCtl(-1);
+        }
+
+        private void DirectoryNumbCtl(int addvalue)
+        {
+            dataHeader.dirNodeNumber += (UInt32)addvalue;
+            dataHeader.fileNodeNumber += (UInt32)addvalue;
+        }
+
+        //ls
+        internal void LookDir(string Terget)
+        {
+
         }
     }
 }
--- Galaxy Plant InＣＲＹＳＴＡＬ・ケイジ/IO/FileFormat/rarc/rarcFormat.cs
+++ Galaxy Plant InＣＲＹＳＴＡＬ・ケイジ/IO/FileFormat/rarc/rarcFormat.cs
@@ -10,51 +10,51 @@ namespace Galaxy_Plant_InＣＲＹＳＴＡＬ_ケイジ.IO.FileFormat.rarc
     {
         struct header //0x20
         {
-            UInt32 mazic;  //0x00
-            UInt32 fileSize;   //0x04
-            UInt32 dataHeaderOffset;   //0x08
-            UInt32 fileDataSectionOffset;  //0x0c
-            UInt32 fileDataSectionLength;  //0x10
-            UInt32 fileDataSectionAllMRAM; //0x14
-            UInt32 fileDataSectionAllARAM; //0x18
-            UInt32 fileDataSectionAllROM;  //0x1c
+            internal UInt32 mazic;  //0x00
+            internal UInt32 fileSize;   //0x04
+            internal UInt32 dataHeaderOffset;   //0x08
+            internal UInt32 fileDataSectionOffset;  //0x0c
+            internal UInt32 fileDataSectionLength;  //0x10
+            internal UInt32 fileDataSectionAllMRAM; //0x14
+            internal UInt32 fileDataSectionAllARAM; //0x18
+            internal UInt32 fileDataSectionAllROM;  //0x1c
         }
 
         struct dataHeader //0x20
         {
-            UInt32 dirNodeNumber;  //0x00
-            UInt32 dirNodeOffset;  //0x04
-            UInt32 fileNodeNumber; //0x08
-            UInt32 fileNodeOffset; //0x0c
-            UInt32 stringNodeNumber;   //0x10
-            UInt32 stringNodeOffset;   //0x14
-            UInt16 nextAvailableFileIndex; //0x18
-            char keepIDSync;  //0x1a
-            char padding1;    //0x1b
-            char padding2;
-            char padding3;
-            char padding4;
-            char padding5;
+            internal UInt32 dirNodeNumber;  //0x00
+            internal UInt32 dirNodeOffset;  //0x04
+            internal UInt32 fileNodeNumber; //0x08
+            internal UInt32 fileNodeOffset; //0x0c
+            internal UInt32 stringNodeNumber;   //0x10
+            internal UInt32 stringNodeOffset;   //0x14
+            internal UInt16 nextAvailableFileIndex; //0x18
+            internal char keepIDSync;  //0x1a
+            internal char padding1;    //0x1b
+            internal char padding2;
+            internal char padding3;
+            internal char padding4;
+            internal char padding5;
         }
 
         struct dirNodeSection //0x10
         {
-            UInt32 nodeNameFirst4String;    //0x00
-            UInt32 nodeNameOffsetIntoStringTable;   //0x04
-            UInt16 nameHash;    //0x08
-            UInt16 fileNodeEntryNumb;  //0x0a
-            UInt32 fileNodeOffset; //0x0c
+            internal UInt32 nodeNameFirst4String;    //0x00
+            internal UInt32 nodeNameOffsetIntoStringTable;   //0x04
+            internal UInt16 nameHash;    //0x08
+            internal UInt16 fileNodeEntryNumb;  //0x0a
+            internal UInt32 fileNodeOffset; //0x0c
         }
 
         struct fileNodeSection //0x10
         {
-            UInt16 nodeIndex;  //0x00
-            UInt16 nameHash;   //0x02
-            char nodeAttributesFlag;  //0x04
-            char padding; //0x05
-            UInt16 nodeNameOffsetIntoStringTable;  //0x06
-            UInt32 dataIndex;  //0x08
-            UInt32 sizeForDataindex;   //0x0c
+            internal UInt16 nodeIndex;  //0x00
+            internal UInt16 nameHash;   //0x02
+            internal char nodeAttributesFlag;  //0x04
+            internal char padding; //0x05
+            internal UInt16 nodeNameOffsetIntoStringTable;  //0x06
+            internal UInt32 dataIndex;  //0x08
+            internal UInt32 sizeForDataindex;   //0x0c
         }
     }
 }
