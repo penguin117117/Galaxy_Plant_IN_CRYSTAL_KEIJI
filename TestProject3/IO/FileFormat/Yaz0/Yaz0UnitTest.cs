f2470ef9f27ee25062e900b58307187242f5db0b
Eigen <70758356+Eigen-aiuHck@users.noreply.github.com>
2022-11-15 22:54:22 +0900

    Fix Encord

--- TestProject3/IO/FileFormat/Yaz0/Yaz0UnitTest.cs
+++ TestProject3/IO/FileFormat/Yaz0/Yaz0UnitTest.cs
@@ -1,8 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Galaxy_Plant_In�b�q�x�r�s�`�k_�P�C�W.IO.FileFormat.Yaz0;
+using Galaxy_Plant_InＣＲＹＳＴＡＬ_ケイジ.IO.FileFormat.Yaz0;
 using System.IO;
 
-namespace Galaxy_Plant_In�b�q�x�r�s�`�k_�P�C�WTest
+namespace Galaxy_Plant_InＣＲＹＳＴＡＬ_ケイジTest
 {
     [TestClass]
     public class Yaz0UnitTest
@@ -10,7 +10,7 @@ namespace Galaxy_Plant_In�b�q�x�r�s�`�k_�P�C�WTest
         [TestMethod]
         public void TestMethod1()
         {
-            const string fileDir = @"E:\�h�L�������g\�Q�[��\�Q�[��rom\Wii\iso\ISO\TakoChuTest\files\ObjectData\";
+            const string fileDir = @"E:\ドキュメント\ゲーム\ゲームrom\Wii\iso\ISO\TakoChuTest\files\ObjectData\";
             Yaz0Decord yaz0Decord = new(fileDir + @"Abekobe2DMoveLift.arc");
             yaz0Decord.Save(fileDir + @"Abekobe2DMoveLift.arc");
             Assert.IsTrue(File.Exists(fileDir + @"Abekobe2DMoveLift.rarc"));
