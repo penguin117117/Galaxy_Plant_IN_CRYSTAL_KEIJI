73d286ea5fad11f742f057bd1cd7f12ae8277a58
ぺんSun <82487890+penguin117117@users.noreply.github.com>
2023-01-05 08:03:58 +0900

    Hashデータを管理するクラスの作成

---
+++ Galaxy Plant InＣＲＹＳＴＡＬ・ケイジ/IO/FileFormat/BCSV/BCSVHash.cs
@@ -0,0 +1,19 @@
+;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy_Plant_InＣＲＹＳＴＡＬ_ケイジ.IO.FileFormat.BCSV
+{
+    internal class BCSVHash
+    {
+        
+    
+
+        internal static void FeatchHashDictionary()
+        {
+
+        }
+
+        
+    }
+}
